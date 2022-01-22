74fabb03dcbfbc24f113bc283e8d7eca86d25de6
Luba <luciano.eq@outlook.com>
2022-01-22 19:41:14 -0300

    :sparkles: adding migrate files versions and database models.

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime
+from email.policy import default
 from sqlalchemy.sql import func
 from src import db
 
@@ -7,8 +9,26 @@ class Model(db.Model):
     __tablename__ = "model"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    modelname = db.Column(db.String(128), nullable=False)
+    modelname = db.Column(db.String(128), nullable=False, unique=True)
+    precision = db.Column(db.Float, nullable=False)
+    recall = db.Column(db.Float, nullable=False)
+    accuracy = db.Column(db.Float, nullable=False)
+    auc = db.Column(db.Float, nullable=False)
+    f1 = db.Column(db.Float, nullable=False)
+    time = db.Column(db.Float, nullable=False)
+    enabled = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, index=True)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    predicts = db.relationship("Predict", backref="model", lazy=True)
 
-    def __init__(self, id, modelname):
-        self.id = id
-        self.modelname = modelname
+
+class Predict(db.Model):
+
+    __tablename__ = "predict"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    transaction = db.Column(db.JSON, nullable=False)
+    prediction = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, index=True)
+    updated_at = db.Column(db.DateTime, default=datetime.now, index=True)
+    model_id = db.Column(db.Integer, db.ForeignKey("model.id"), nullable=False)
